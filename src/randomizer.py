e3aa4811259388e7a52737af8c3ae05a45a49a83
Jason <yugiohduel3@gmail.com>
2021-07-30 14:07:09 +0200

    added some more stuff

--- src/randomizer.py
+++ src/randomizer.py
@@ -1,2 +1,21 @@
-import os
+import os, json
 
+def list_items():
+    files = []
+    for file in os.listdir("sample-skin"):
+	    files.append(file)
+    return files
+ 
+items = list_items()
+
+configs = 0
+with open("config/config.json") as f:
+    configs = json.load(f)
+
+skins_folders = []
+try:
+    skins_folders = os.listdir(configs(f))
+except:
+    print("error with the skins folder")
+
+randomized_skin = []
